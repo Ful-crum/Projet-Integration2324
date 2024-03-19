739ff7c9f0fce27ac7ab663f4d9775eb40093d64
Edward Abrahamian <Abrahamian.Edward@hotmail.com>
2024-03-19 19:21:10 +0100

    Models, controllers, templates, routes for Type, Locality and Role

--- reservations/urls.py
+++ reservations/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('catalogue.urls')),
     path('catalogue/', include('catalogue.urls')),
     path('admin/', admin.site.urls),
 ]
