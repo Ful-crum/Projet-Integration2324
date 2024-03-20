01805fe7783217e77e4351b974259fb26cd2c6db
Edward Abrahamian <Abrahamian.Edward@hotmail.com>
2024-03-20 01:43:35 +0100

    Restful API install

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -4,28 +4,7 @@ from django.urls import path
 from .views import *
 from . import views
 
-from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
 app_name='catalogue'
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
 
 urlpatterns = [
     path('artist/', views.artist.index, name='artist_index'),
@@ -42,8 +21,5 @@ urlpatterns = [
     path('show/<int:show_id>', views.show_.show, name='show_show'),
     path('representation/', views.representation.index, name='representation_index'),
     path('representation/<int:show_id>', views.representation.show, name='representation_show'),
-
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
